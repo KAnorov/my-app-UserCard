569bb72feb5e5bd32f5aadf7f2d68b49cdf7df86
KAnorov <kostya-anorov046@yandex.ru>
2024-11-08 20:32:59 +0300

    Добавил Spinner

--- components/GetUsers.jsx
+++ components/GetUsers.jsx
@@ -1,101 +1,129 @@
 import { useEffect, useState } from 'react';
 import UserForm from './UserForm';
 import UserList from './UserList';
+import Spinner from './Spinner';
+
+
 const
     endpoint = 'http://localhost:3333/users';
 
 export default function GetUsers() {
-    const [users, setUsers] = useState([]);
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [isEdit, setIsEdit] = useState(false);
-    const [editId, setEditId] = useState(null);
+    const
+        [users, setUsers] = useState([]),
+        [name, setName] = useState(''),
+        [email, setEmail] = useState(''),
+        [phone, setPhone] = useState(''),
+        [isEdit, setIsEdit] = useState(false),
+        [editId, setEditId] = useState(null),
+        [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const response = await fetch('http://localhost:3333/users');
-                if (!response.ok) throw new Error('Ошибка загрузки пользователей');
-                const data = await response.json();
-                setUsers(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
+        const
+            fetchUsers = async () => {
+                try {
+                    const
+                        response = await fetch('http://localhost:3333/users');
+                    if (!response.ok) throw new Error('Ошибка загрузки пользователей');
+                    const
+                        data = await response.json();
+                    setUsers(data);
+                } catch (error) {
+                    console.error(error);
+                }
+            };
 
         fetchUsers();
     }, []);
 
-    const handleDelete = async (id) => {
-        try {
-            const response = await fetch(`${endpoint}/${id}`, { method: 'DELETE' });
-            if (response.ok) {
-                setUsers(users.filter(user => user.id !== id));
-            } else {
-                console.error(`Не удалось удалить пользователя с ID: ${id}`);
-            }
-        } catch (error) {
-            console.error('Ошибка при удалении: ', error);
-        }
-    };
-
-    const handleAdd = async () => {
-        const newUser = { name, email, phone, };
-        try {
-            const response = await fetch('http://localhost:3333/users', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(newUser),
-            });
-            if (!response.ok) {
-                throw new Error(`Ошибка при добавлении пользователя: ${response.status}`);
-            }
-            const data = await response.json();
-            setUsers([...users, data]);
-            resetForm();
-        } catch (error) {
-            console.error('Ошибка при добавлении пользователя: ', error);
-        }
-    };
-
-    const handleEdit = (id) => {
-        const user = users.find(user => user.id === id);
-        if (user) {
-            setName(user.name);
-            setEmail(user.email);
-            setPhone(user.phone);
-            setIsEdit(true);
-            setEditId(id);
-        }
-    };
-
-    const handleSave = async () => {
-        if (editId !== null) {
-            const updatedUser = { name, email, phone };
+    const
+        handleDelete = async (id) => {
             try {
-                const response = await fetch(`http://localhost:3333/users/${editId}`, {
-                    method: 'PUT',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(updatedUser),
-                });  
-                if (!response.ok) {
-                    throw new Error(`Ошибка при сохранении изменений: ${response.status}`);
+                const
+                    response = await fetch(`${endpoint}/${id}`, { method: 'DELETE' });
+                if (response.ok) {
+                    setUsers(users.filter(user => user.id !== id));
+                } else {
+                    console.error(`Не удалось удалить пользователя с ID: ${id}`);
                 }
- 
-                setUsers(users.map(user => user.id === editId ? { ...user, ...updatedUser } : user));
-                resetForm();
             } catch (error) {
-                console.error('Ошибка при сохранении изменений: ', error);
+                console.error('Ошибка при удалении: ', error);
+                
             }
-        }
-    };
+        };
+
+    const
+        handleAdd = async () => {
+            const
+                newUser = { name, email, phone, };
+            setLoading(true);
+            try {
+                const
+                    response = await fetch('http://localhost:3333/users', {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(newUser),
+                    });
+                if (!response.ok) {
+                    throw new Error(`Ошибка при добавлении пользователя: ${response.status}`);
+                    
+                }
+                const
+                    data = await response.json();
+                setUsers([...users, data]);
+                resetForm();
+                setLoading(false);
+            } catch (error) {
+                console.error('Ошибка при добавлении пользователя: ', error);
+                setLoading(false);
+            }
+        };
+
+    const
+        handleEdit = (id) => {
+            const
+                user = users.find(user => user.id === id);
+            if (user) {
+                setName(user.name);
+                setEmail(user.email);
+                setPhone(user.phone);
+                setIsEdit(true);
+                setEditId(id);
+            }
+        };
+
+    const
+        handleSave = async () => {
+            if (editId !== null) {
+                const
+                    updatedUser = { name, email, phone };
+                setLoading(true);
+                try {
+                    const
+                        response = await fetch(`http://localhost:3333/users/${editId}`, {
+                            method: 'PUT',
+                            headers: { 'Content-Type': 'application/json' },
+                            body: JSON.stringify(updatedUser),
+                        });
+                    if (!response.ok) {
+                        throw new Error(`Ошибка при сохранении изменений: ${response.status}`);
+                    }
+
+                    setUsers(users.map(user => user.id === editId ? { ...user, ...updatedUser } : user));
+                    resetForm();
+                } catch (error) {
+                    console.error('Ошибка при сохранении изменений: ', error);
+                    setLoading(false);
+                }
+            }
+        };
 
     const resetForm = () => {
         setName('');
         setEmail('');
         setPhone('');
         setIsEdit(false);
+        setEditId(null);
+        setLoading(false);
 
     };
 
@@ -115,6 +143,7 @@ export default function GetUsers() {
     };
 
     return <>
+        {loading && <Spinner />}
         <div className='forma'>
             <UserForm
                 name={name}
@@ -128,6 +157,7 @@ export default function GetUsers() {
                 users={users}
                 handleEdit={handleEdit}
                 handleDelete={handleDelete} /></div>
+
     </>;
 };
 
--- components/UserForm.jsx
+++ components/UserForm.jsx
@@ -2,9 +2,9 @@ export default function UserForm({ name, email, phone, isEdit, handleChange, han
 
     return <fieldset>
         <legend>{isEdit ? 'Редактирование пользователя' : 'Добавление пользователя'}</legend>
-        <input type="text" value={name} onChange={handleChange('name')} placeholder="Имя" /> <br />
-        <input type="email" value={email} onChange={handleChange('email')} placeholder="Email" /><br />
-        <input type="tel" value={phone} onChange={handleChange('phone')} placeholder="Телефон" /><br />
+        <input value={name} onChange={handleChange('name')} placeholder="Имя" /> <br />
+        <input value={email} onChange={handleChange('email')} placeholder="Email" /><br />
+        <input value={phone} onChange={handleChange('phone')} placeholder="Телефон" /><br />
         <button onClick={handleSubmit}>{isEdit ? 'Сохранить' : 'Добавить'}</button>
     </fieldset>;
 }
